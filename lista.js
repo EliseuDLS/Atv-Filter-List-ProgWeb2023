7201b0d1332766720739fbdb4d9eafc689721251
EliseuDLS <124800294+EliseuDLS@users.noreply.github.com>
2023-04-14 14:54:07 -0300

    Update lista.js

--- lista.js
+++ lista.js
@@ -1,5 +1,7 @@
 const nameList = ["Eliseu", "Lelebis", "Naras", "Vara", "Pedrinho", "Batman", "Luiz"]
 
+var num_seleciona = -1;
+
 const listEl = document.getElementById("list");
 const searchField = document.getElementById("searchField");
 
@@ -23,10 +25,17 @@ function fillList(list = nameList) {
     listEl.innerHTML = "";
     for(let i = 0; i < list.length; i++) {
         let listItems = document.createElement("li");
+        if (num_seleciona == i){
+            listItems.className = "alert alert-dark";
+        } else{
+            listItems.className = "";
+        }
         listItems.innerHTML = list[i];
         listEl.appendChild(listItems);
     }
 }
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function inputHandler() {
     saida.innerHTML = ""
@@ -42,7 +51,6 @@ function inputHandler() {
 
 function addNome(){
     nome = searchField.value;
-
     if(nome != ''){
         nameList.push(nome);
         saida.innerHTML = 'O nome "' + nome + '" foi adicionado!';
@@ -56,7 +64,7 @@ function addNome(){
 function removerNome(){
     nome = searchField.value; 
     var encontrar = nameList.indexOf(nome); 
-    if (encontrar != -1){
+    if(encontrar != -1){
         nameList.splice(encontrar, 1);  
         saida.innerHTML = 'O nome "' + nome + '" foi removido!';
     }else{
@@ -66,31 +74,12 @@ function removerNome(){
 //////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 function selecionarNome(){
-
-    const listaNomes  = document.querySelectorAll("li")  // obtém tags h5 da página
-    console.log = (listaNomes);
-  
-    if (listaNomes.length == 0) {
-        saida.innerHTML = 'Não há tarefas para selecionar'       // se não há tarefas, exibe alerta
-      return                                        // e retorna
+    var aux = nameList[num_seleciona]
+    if(nameList.length == num_seleciona){
+        num_seleciona = 0;
+    }else{
+    num_seleciona += 1;
     }
-  
-    let aux = -1                   // variável auxiliar para indicar linha selecionada
-  
-    // percorre a lista de elementos h5 inseridos na página, ou seja, tarefas
-    for (let i = 0; i < listaNomes.length; i++) {
-      // se tag é da class tarefa-selecionada (está selecionada)
-      if (listaNomes[i].className == "tarefa-selecionada") {
-        listaNomes[i].className = "tarefa-normal"      // troca para normal
-        aux = i                                     // muda valor da variável auxiliar
-        break                                       // sai da repetição
-      }
-    }
-  
-    // se a linha que está selecionada é a última, irá voltar para a primeira
-    if (aux == listaNomes.length - 1) {
-      aux = -1
-    }
-  
-    listaNomes[aux + 1].className = "tarefa-selecionada" // muda estilo da próxima linha
-}  
+    console.log(num_seleciona);
+    inputHandler()
+}
